2fab345f90a53895b00e1ee1d326c49b6857b06e
waseem81621 <waseemaiintegration@gmail.com>
2026-06-24 00:22:47 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/canindigo-cinematic/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
